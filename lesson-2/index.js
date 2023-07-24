e026dc649c5884eca9254219ef2cfd463a32e630
NathanCao0111 <nhatcao0111@gmail.com>
2023-07-24 09:31:31 +0700

    update lesson-2

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -1,16 +1,142 @@
-
-const express = require('express')
-const app = express()
-const port = 5000
-const route = require('./routes')
+const http = require("http");
+// const express = require("express");
+// const app = express();
+// const port = 5000;
+// const route = require("./routes");
 
 // config res.body
-app.use(express.urlencoded({
-	extended: true
-})) //form data
-app.use(express.json()) //js, json data
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// ); //form data
+// app.use(express.json()); //js, json data
 
 // route init
-route(app)
+// route(app);
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+// app.listen(port, () =>
+//   console.log(`App listening at http://localhost:${port}`)
+// );
+
+// create server
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   res.end(JSON.stringify({
+// 			data: 'Hello world'
+// 		}));
+// });
+
+// server.listen(8080, () => console.log(`Server is running at http://localhost:8080`))
+
+// create web application
+
+// const express = require("express");
+// const app = express();
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   console.log(req);
+//   res.end(`<h2>Hello</h2>
+// 		<p>This is ${req.method} method in URL ${req.url} with status code ${req.statusCode}
+// 		`);
+// });
+
+// app.get("/product/:id", (req, res) => {
+//   // console.log(req);
+//   res.end(`<h2>Product</h2>
+// 		<p>This is ${req.method} method in URL ${req.url} with status code ${req.statusCode}
+// 		<p>Param id: ${req.params.id}</p>
+// 		`);
+// });
+
+// app.post("/auth/signin", (req, res) => {
+//   console.log(req.body);
+//   res.send("product posted");
+// });
+
+// const hello = (req, res, next) => {
+//   console.log("hello");
+//   next();
+// };
+
+// const xinChao = (req, res, next) => {
+//   console.log("xin chao");
+//   next();
+// };
+
+// app.get("/products", hello, xinChao, (req, res) => {
+//   console.log("req query: ", req.query);
+//   res.send("product query has arrived");
+// });
+
+// app.listen(8000, () =>
+//   console.log(`Server is running at http://localhost:8000`)
+// );
+
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
+
+app.use(express.json());
+
+// req.params
+const todoList = [
+  {
+    id: crypto.randomUUID(),
+    todoName: "Chores",
+    date: new Date(),
+    status: "Pending",
+  },
+];
+
+app.get("/api/v1/todo-list", (req, res) => {
+  try {
+    const queryParams = req.query;
+    const getTodoByFields = todoList.map((element) => {});
+  } catch (error) {
+    res.send({
+      data: null,
+      message: "Failed",
+      success: false,
+    });
+  }
+});
+
+app.post("/api/v1/todo-list", (req, res) => {
+  const dataBody = req.body;
+		todoList.push(dataBody)
+		res.send({
+			data: todoList,
+			message: 'Success',
+			success: true
+		})
+});
+
+app.get("/api/v1/todo-list/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    // null, undefined, empty string, NaN, 0, false
+    const findRecordTodo = todoList.find((element) => element.id === id);
+    if (!findRecordTodo) {
+      throw new Error("Cannot find todo");
+    }
+    res.send({
+      data: findRecordTodo,
+      message: "Success",
+      success: true,
+    });
+  } catch (error) {
+    res.send({
+      data: null,
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
+app.listen(8000, () =>
+  console.log(`Server is running at http://localhost:8000`)
+);
